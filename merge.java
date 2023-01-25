fb18c32b2119ceb95d8b9643e9bd67e55c6b2527
Ayush-Anshuli <109850987+Ayush-Anshuli@users.noreply.github.com>
2023-01-25 20:57:56 +0530

    Add files via upload

---
+++ merge.java
@@ -0,0 +1,60 @@
+import java.util.*;
+public class merge {
+    public static void mergesort(int arr[],int si,int ei){
+        if(si>=ei){
+            return;
+        }
+        int mid=si+(ei-si)/2;
+        mergesort(arr, si, mid);
+        mergesort(arr, mid+1, ei);
+        merge(arr,si,mid,ei);
+    }
+    public static void merge(int arr[],int si,int mid,int ei){
+        int temp[]=new int[ei-si+1];
+        int i=si;
+        int j=mid+1;
+        int k=0;
+        while(i<=mid && j<=ei){
+            if(arr[i]<arr[j]){
+                temp[k]=arr[i];
+                i++;
+            }
+            else{
+                temp[k]=arr[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<=mid){
+            temp[k++]=arr[i++];
+        }
+        while(j<=ei){
+            temp[k++]=arr[j++];
+        }
+
+        for(k=0,i=si;k<temp.length;i++,k++){
+            arr[i]=temp[k];
+        }
+    }
+
+    public static void print(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of array");
+        int a=sc.nextInt();
+
+        int arr[]=new int[a];
+        System.out.println("enter the element");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        mergesort(arr, 0, arr.length-1);
+        print(arr);
+    }
+}
